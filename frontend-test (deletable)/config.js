53cc44ddc208f959f8cf5bc107e9426752449818
Ahmed Nader <ahmedelrafe@gmail.com>
2026-08-02 17:37:01 +0300

    Delete frontend-test (deletable) directory

--- frontend-test (deletable)/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-// -----------------------------------------------------------------------------
-// The ONLY link between this frontend and the backend.
-// If your backend runs on a different address or port, change it here.
-// -----------------------------------------------------------------------------
-
-const API_BASE = "http://localhost:3000/api";
